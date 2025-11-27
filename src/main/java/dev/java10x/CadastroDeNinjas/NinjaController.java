42451566628758cc99c89cf3bb3f1ee408faa8bd
Vitor Alves <vitor.alves07750@outlook.com>
2025-11-27 15:41:09 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -1,13 +1,12 @@
 package dev.java10x.CadastroDeNinjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
