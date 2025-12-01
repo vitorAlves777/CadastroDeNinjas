e571d184414644f2591cb1c548cdcc6060f33ee3
Vitor Alves <vitor.alves07750@outlook.com>
2025-12-01 18:15:04 -0300

    feat: Coloca o lombok na Classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
 
     @Id
@@ -18,39 +24,4 @@ public class MissaoModel {
     // Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninja;
-
-    public MissaoModel() {
-    }
-
-    public MissaoModel(String nome, String rank) {
-        this.nome = nome;
-        this.rank = rank;
-    }
-
-    public MissaoModel(String nome, String rank, List<NinjaModel> ninja) {
-        this.nome = nome;
-        this.rank = rank;
-        this.ninja = ninja;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table( name = "tb_cadastro" )
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -20,38 +26,4 @@ public class NinjaModel {
     @JoinColumn( name = "missao_id") // Foreing Key ou chave estrangeira
     private MissaoModel missao;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
