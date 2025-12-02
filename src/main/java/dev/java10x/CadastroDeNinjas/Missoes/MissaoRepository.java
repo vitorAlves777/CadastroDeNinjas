0890788da53fffc6220b1a2bdbf3f15a6da68588
vitorAlves777 <vitor.fer.alves7@gmail.com>
2025-12-02 11:20:58 -0300

    feat: Cria Repository de missões

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,8 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+
+
+}
