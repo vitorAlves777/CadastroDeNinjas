a6bda10facb25cdf45e07c5f41f53491256ccbce
vitorAlves777 <vitor.fer.alves7@gmail.com>
2025-12-06 07:23:54 -0300

    feat: Adiciona um Controller a rota base

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/HomeController.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+}
